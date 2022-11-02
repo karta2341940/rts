a9c7bcecabb8a251c2c1a16a9530448b83cc982b
karta2341940 <a08487a@gmail.com>
2022-11-02 22:34:08 +0800

    exact,utilization ok

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ main();
 
 
 function main() {
-    console.clear();
     // Declare variables file
     let file = new Array();
     try {
@@ -38,31 +37,42 @@ function main() {
     //console.log(roundTimes);
     //console.log(tasks);
     exactTest(tasks);
+    utilizationTest(tasks);
 }
 
 function exactTest(tasks = [{ "ID": 0, "Period": 0, "ComputationalTime": 0, }]) {
-    console.clear();
     let n = tasks.length;
-    console.log("Len : ", n);
+    let sum = new Array();
     for (let taskSet = 0; taskSet < n; taskSet++) {
-        let sum = 0;
-        let sum2 = 0;
+        let temp2 = 0;
         for (let round = 0; round <= taskSet; round++) {
-
+            sum[taskSet] = new Array();
+            // Initial rn
+            let temp = 0;
             if (round == 0) {
                 for (let c = 0; c <= taskSet; c++) {
-                    sum += parseInt(tasks[c].ComputationalTime);
+                    let Cj = parseInt(tasks[c].ComputationalTime);
+                    temp += parseInt(Cj);
                 }
+                temp2 = temp;
             }
             else {
                 for (let c = 0; c <= taskSet; c++) {
-                    // TODO:rn=sum(cj*ceiling(rn-1/pj))
-                    sum2 += parseInt(tasks[c].ComputationalTime);
+
+                    let Cj = parseInt(tasks[c].ComputationalTime);
+                    let Pj = parseInt(tasks[c].Period);
+                    let round_minus_1 = parseInt(temp2);
+                    temp += parseInt(Cj * ceiling(round_minus_1 / Pj));
                 }
             }
+            sum[taskSet][round] = temp;
+            if (sum[taskSet][round] > tasks[taskSet].Period) {
+                return false,console.log("faild( exact test )");
+            }
+            //console.log("T", taskSet, "\t round : ", round, "\tSum : ", sum[taskSet][round])
         }
-        console.log("Sum : ", sum, "\t Sum2", sum2)
     }
+    return true,console.log("success( exact test )");
 }
 function utilizationTest(tasks = [{ "ID": 0, "Period": 0, "ComputationalTime": 0, }]) {
     let n = Number(tasks.length);
@@ -71,14 +81,16 @@ function utilizationTest(tasks = [{ "ID": 0, "Period": 0, "ComputationalTime": 0
     for (let i = 0; i < n; i++) {
         let cI = Number(tasks[i].ComputationalTime);
         let pI = Number(tasks[i].Period);
-        sum = parseFloat(cI / pI);
+        sum += parseFloat(cI / pI);
     }
-    if (sum <= U) return true;
-    else return false;
+    console.log("U",U)
+    if (sum <= U) return true,console.log("success( utilization test )",sum);
+    else return false,console.log("fail( utilization test )",sum);
 }
 
-function ceiling(number=Number){
-    return Math.ceil(number);
+function ceiling(number = Number) {
+    //console.log(number)
+    return parseInt(Math.ceil(number));
 }
 
 /**
